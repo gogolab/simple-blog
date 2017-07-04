c09114e1cd99e69f484c981ebb054fb2ac6798f1
gogolab <m4ttek@gmail.com>
2017-07-04 22:26:30 +0200

    changed articles controller

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -5,17 +5,17 @@ exports.list_articles = function(req, res) {
 };
 
 exports.create_article = function(req, res) {
-    res.send("to do: create article");
+  res.send("to do: create article");
 };
 
 exports.read_article = function(req, res) {
-    res.json({title: "Very first title", body: "Lorem ipsum dolor sit amet"});
+  res.json({title: "Very first title", body: "Lorem ipsum dolor sit amet"});
 };
 
 exports.update_article = function(req, res) {
-    res.send("to do: update article");
+  res.send("to do: update article");
 };
 
 exports.delete_article = function(req, res) {
-    res.send("to do: delete article");
+  res.send("to do: delete article");
 };
