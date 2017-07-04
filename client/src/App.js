198cf067d050031afcbedbb7ed6b67d402451446
gogolab <m4ttek@gmail.com>
2017-07-04 17:15:12 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {users: []};
+  };
+
+  componentDidMount() {
+    fetch("/users")
+      .then(res => res.json())
+      .then(users => this.setState({users}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h2>Users:</h2>
+            {this.state.users.map( user => {
+                return <div key={user.id}>{user.username}</div>;
+              })
+            }
+      </div>
+    );
+  }
+}
+
+export default App;
