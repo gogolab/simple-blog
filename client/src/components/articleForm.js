8da81ae97aad7a1e5cbd3104fae40cfa57aabe16
gogolab <m4ttek@gmail.com>
2017-07-06 20:39:42 +0200

    updated article form

--- client/src/components/articleForm.js
+++ client/src/components/articleForm.js
@@ -4,16 +4,26 @@ import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap
 const ArticleForm = () => (
   <div>
     <Form>
-      <FormGroup row>
-        <Label for="article__title"  sm={2}>Title</Label>
-        <Col sm={10}>
-          <Input type="text" name="title" id="article__title" placeholder="" />
-        </Col>
+      <FormGroup>
+        <Label for="article__title">Title:</Label>
+        <Input type="text" name="title" id="article__title" required />
       </FormGroup>
       <FormGroup>
-          <Label for="article__body">Body</Label>
-          <Input type="textarea" name="body" id="article__body" />
+        <Label for="article__body">Body:</Label>
+        <Input type="textarea" name="body" id="article__body" />
       </FormGroup>
+      <FormGroup>
+        <Label for="image__url">Image URL:</Label>
+        <Input type="url" name="url" id="image__url" />
+      </FormGroup>
+      <FormGroup>
+          <Label for="article__author">Author:</Label>
+          <Input type="select" name="author" id="article__author">
+            <option>John Doe</option>
+            <option>Jane Nodoe</option>
+          </Input>
+        </FormGroup>
+      <Button>Submit</Button>
     </Form>
   </div>
 );
