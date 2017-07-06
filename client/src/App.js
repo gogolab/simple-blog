2a392df303bebd9f2adaf30e696f6904a7f29967
gogolab <m4ttek@gmail.com>
2017-07-06 13:21:54 +0200

    hotfix

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,19 @@ import { Articles } from './components/articles';
 import { Article } from './components/article';
 import { AddArticle } from './components/addArticle';
 import { PageNotFound } from './components/pageNotFound';
+import { Container, Row, Col } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    
+    this.toggle = this.toggle.bind(this);
 
     this.state = {
       users: [],
-      article: {}
+      article: {},
+      isOpen: false
     };
   };
 
@@ -26,39 +31,47 @@ class App extends Component {
       .then(res => res.json())
       .then(article => this.setState({article}));
   }
+  
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
 
   render() {
     return (
       <div className="App">
-        <div className="wrapper">
+      
+        <Navbar color="faded" light toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand href="/">Simple Blog</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink tag={Link} to="/articles">Articles</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/add-new-article">Add Article</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
 
-              <div className="row center-xs">
-                <div className="col-xs-12">
-                  <div className="box">
-                    <ul>
-                      <li><Link to="/">Home</Link></li>
-                      <li><Link to="/articles">Articles</Link></li>
-                      <li><Link to="/add-new-article">Add Article</Link></li>
-                    </ul>
-                  </div>
-                </div>
+        <Container>
+          <Row>
+            <Col>
+              <div className="box">
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route path="/articles" component={Articles} />
+                  <Route path="/article/:id" component={Article} />
+                  <Route path="/add-new-article" component={AddArticle} />
+                  <Route component={PageNotFound} />
+                </Switch>
               </div>
-                <hr/>
-              <div className="row">
-                <div className="col-xs-12">
-                  <div className="box">
-                    <Switch>
-                      <Route exact path="/" component={Home} />
-                      <Route path="/articles" component={Articles} />
-                      <Route path="/article/:id" component={Article} />
-                      <Route path="/add-new-article" component={AddArticle} />
-                      <Route component={PageNotFound} />
-                    </Switch>
-                  </div>
-                </div>
-              </div>
-
-        </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
