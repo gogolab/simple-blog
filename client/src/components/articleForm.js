bc1f7a5c97f1f90b9e8ac3d0939603e7dbc150db
gogolab <m4ttek@gmail.com>
2017-07-11 17:28:23 +0200

    changed add form

--- client/src/components/articleForm.js
+++ client/src/components/articleForm.js
@@ -8,13 +8,27 @@ const ArticleForm = () => (
         <Label for="article__title">Title:</Label>
         <Input type="text" name="title" id="article__title" required />
       </FormGroup>
+      <FormGroup row>
+        <legend className="col-form-legend col-sm-12">Main Image</legend>
+        <Col xs="6" sm="4">
+          <FormGroup>
+            <img src="http://via.placeholder.com/300x200" width="100%" className="img-fluid"/>
+          </FormGroup>
+        </Col>
+        <Col xs="12" sm="8">
+          <FormGroup>
+            <Label for="image__url">Image URL:</Label>
+            <Input type="url" name="image__url" id="image__url" />
+            <Label for="image__title">Image Title:</Label>
+            <Input type="url" name="image__title" id="image__title" />
+            <Label for="image__alt">Image Alt decription:</Label>
+            <Input type="url" name="image__url" id="image__alt" />
+          </FormGroup>
+        </Col>
+      </FormGroup>
       <FormGroup>
         <Label for="article__body">Body:</Label>
         <Input type="textarea" name="body" id="article__body" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="image__url">Image URL:</Label>
-        <Input type="url" name="url" id="image__url" />
       </FormGroup>
       <FormGroup>
           <Label for="article__author">Author:</Label>
