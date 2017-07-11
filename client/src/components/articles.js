947bec88c1b732772663b425e7f9b87f8f24e17d
gogolab <m4ttek@gmail.com>
2017-07-11 12:47:31 +0200

    cleaned styling code

--- client/src/components/articles.js
+++ client/src/components/articles.js
@@ -41,27 +41,92 @@ export class Articles extends Component {
               
               if(index === 0) {
                 return (
-                  <Col sm={12} lg={8} style={{display: "flex", flexDirection: "column"}}>
-                    <Card inverse tag={Link} to={articleUrl} style={{marginBottom: "30px"}}>
-                      <CardImg top width="100%" src={article.imageUrl} style={{}}  alt="Card image cap" />
-                      <CardImgOverlay style={{display: "flex", flexDirection: "column-reverse", justifyContent: "flex-start", background: "linear-gradient(to top, hsl(0, 0%, 0%), hsla(44.3, 100%, 45.1%, 0.1) 60%, hsla(44.3, 100%, 45.1%, 0.2)"}}>
-                        <CardTitle style={{textTransform: "uppercase"}}>{article.title}</CardTitle>
-                        <div style={{textTransform: "uppercase", color: "#aaa", fontSize: "0.75em"}}>july 6, 2017</div>
+                  <Col sm={12} lg={8} style={{
+                    display: "flex",
+                    flexDirection: "column"
+                  }}>
+                    <Card
+                      inverse
+                      tag={Link}
+                      to={articleUrl}
+                      style={{
+                        marginBottom: "30px"
+                      }}
+                    >
+                      <CardImg
+                        top
+                        width="100%"
+                        src={article.imageUrl}
+                        alt="Card image cap"
+                      />
+                      <CardImgOverlay style={{
+                          display: "flex",
+                          flexDirection: "column-reverse",
+                          justifyContent: "flex-start",
+                          background: "linear-gradient(to top, hsl(0, 0%, 0%), hsla(44.3, 100%, 45.1%, 0.1) 60%, hsla(44.3, 100%, 45.1%, 0.2)"
+                      }}>
+                        <CardTitle style={{textTransform: "uppercase"}}>
+                          {article.title}
+                        </CardTitle>
+                        <div style={{
+                          textTransform: "uppercase",
+                          color: "#aaa",
+                          fontSize: "0.75em"
+                        }}>
+                          july 6, 2017
+                        </div>
                       </CardImgOverlay>
                     </Card>
                   </Col>
                 );
               } else {
                 return (
-                  <Col sm={12} md={6} lg={4} style={{display: "flex", flexDirection: "column"}}>
-                    <Card tag={Link} to={articleUrl} style={{marginBottom: "30px", textDecoration: "none", color: "black"}}>
-                      <CardImg top width="100%" src={article.imageUrl} style={{objectFit: "cover", maxHeight: "200px"}}  alt="Card image cap" />
-                      <CardBlock style={{display: "flex", flexFlow: "column nowrap", justifyContent: "flex-start", minHeight: "150px"}}>
-                        <div style={{textTransform: "uppercase", color: "#aaa", fontSize: "0.75em"}}>july 6, 2017</div>
-                        <CardTitle style={{textTransform: "uppercase"}}>{article.title}</CardTitle>
+                  <Col sm={12} md={6} lg={4} style={{
+                    display: "flex",
+                    flexDirection: "column"
+                  }}>
+                    <Card
+                      tag={Link}
+                      to={articleUrl}
+                      style={{
+                        marginBottom: "30px",
+                        textDecoration: "none",
+                        color: "black"
+                      }}
+                    >
+                      <CardImg
+                        top
+                        width="100%"
+                        src={article.imageUrl}
+                        alt="Card image cap"
+                        style={{
+                          objectFit: "cover",
+                          maxHeight: "200px"
+                        }}
+                      />
+                      <CardBlock style={{
+                        display: "flex",
+                        flexFlow: "column nowrap",
+                        justifyContent: "flex-start",
+                        minHeight: "150px"
+                      }}>
+                        <div style={{
+                          textTransform: "uppercase",
+                          color: "#aaa",
+                          fontSize: "0.75em"
+                        }}>
+                          july 6, 2017
+                        </div>
+                        <CardTitle style={{textTransform: "uppercase"}}>
+                          {article.title}
+                        </CardTitle>
                       </CardBlock>
                       <CardFooter>
-                        <img src="http://i.imgur.com/fzwK2ap.jpg" width="40px" style={{borderRadius: "50%"}} />
+                        <img
+                          src="http://i.imgur.com/fzwK2ap.jpg"
+                          width="40px"
+                          style={{borderRadius: "50%"}}
+                        />
                       </CardFooter>
                     </Card>
                   </Col>
